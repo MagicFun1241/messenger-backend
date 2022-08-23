f70298d3bf51f4ca0d4d65d60c088564a6f4854e
magicfun <nganzikov@gmail.com>
2022-08-23 11:59:48 +0300

    Update users mock

--- src/users/__mocks__/users.service.ts
+++ src/users/__mocks__/users.service.ts
@@ -1,7 +1,7 @@
 import { userStub } from '../tests/stubs/user.stub';
 
 export const UsersService = jest.fn().mockReturnValue({
-  findOne: jest.fn().mockResolvedValue(userStub()),
+  findById: jest.fn().mockResolvedValue(userStub()),
   create: jest.fn().mockResolvedValue(userStub()),
   findByExternalId: jest.fn().mockResolvedValue(userStub()),
   findByExternalIdOrCreate: jest.fn().mockResolvedValue(userStub()),

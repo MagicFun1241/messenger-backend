82565f6a3e44d931ff43f9cb6591201edf18cf2a
int1m <mostovoy@volsu.ru>
2022-08-25 16:46:52 +0300

    fix: newTokenExternal create

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -72,7 +72,6 @@ export class AuthenticationService {
       service: createTokenExternalDto.service,
       ip: createTokenExternalDto.ip,
     });
-    await newTokenExternal.save();
 
     return newTokenExternal.token;
   }

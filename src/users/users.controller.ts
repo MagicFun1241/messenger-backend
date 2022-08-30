2d3ef0118d84f1e14ea245d47a37b72db2871c64
magicfun <nganzikov@gmail.com>
2022-08-30 11:15:14 +0300

    Implement private createUser method for http server

---
+++ src/users/users.controller.ts
@@ -0,0 +1,27 @@
+import {Body, Controller, HttpException, HttpStatus, Post} from '@nestjs/common';
+import { WsFormatException } from '@/ws/exceptions/ws.format.exception';
+import { PrivateCreateUser } from '@/users/dto/privateCreateUser';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '@/users/users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  // Приватный метод, аналогичный методу на сокет-сервере
+  @Post('/')
+  async createUser(@Body() body: PrivateCreateUser) {
+    if (
+      this.configService.get('PRIVATE_ROUTES_ENABLED') !== 'true'
+      || body.secret !== this.configService.get('PRIVATE_ROUTES_SECRET')
+    ) {
+      throw new HttpException('Access denied', HttpStatus.FORBIDDEN);
+    }
+
+    const item = await this.usersService.create(body);
+    return { id: item._id.toString() };
+  }
+}
